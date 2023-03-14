6549b8b7c207edbc446e667c7de4579cdd88dcab
Zachary <zachary@Zipcoders-MacBook-Pro-12.local>
2023-03-14 13:26:42 -0400

    i have made changes

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,50 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private double height;
+    private boolean male;
+    private boolean happy;
+    private String hairColor;
+    private String eyeColor;
 
     public Person() {
+    this.name = "";
+    this.age = Integer.MAX_VALUE;
+    this.height = 62;
+    this.male = true;
+    this.happy = true;
+    this.hairColor = "brown";
+    this.eyeColor = "blue";
     }
 
     public Person(int age) {
+        name = "";
+        setAge(age);
     }
 
     public Person(String name) {
+        this.name = "Leon";
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(String name, int age) {
+        this.name = "Leon";
+        this.age = 5;
     }
 
-    public void setName(String name) {
+    public String setName(String name) {
+        return this.name = name;
     }
 
-    public void setAge(int age) {
+    public int setAge(int age) {
+        return this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
